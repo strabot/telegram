44cf2c689af2c511eb2b4258cca7c0be12c10e69
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-02 11:56:29 -0300

    Initial service StrabotManager

---
+++ src/services/StrabotManager.js
@@ -0,0 +1,10 @@
+import got from 'got'
+
+const { STRABOT_MANAGER_URL, STRABOT_MANAGER_TOKEN } = process.env
+
+export const StrabotManager = got.extend({
+  prefixUrl: STRABOT_MANAGER_URL,
+  headers: {
+    Authorization: STRABOT_MANAGER_TOKEN
+  }
+})
