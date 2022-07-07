96453566b9a9a18cbd7f6be88edde814f351e089
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-07 20:19:50 -0300

    Initial setup for schedules

---
+++ src/bootstrap/schedules.js
@@ -0,0 +1,9 @@
+/**
+ * Schedules bootstrap
+ * @param {Object} params
+ * @param {import('cron').CronJob} params.CronJob
+ * @param {import('got').Got} params.StrabotManager
+ * @param {import('telegraf').Telegraf} params.bot
+ */
+export async function schedules({ CronJob, StrabotManager, bot }) {
+}
