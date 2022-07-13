8bf4b2045669db2a29cf1e32389c35b3a8c23c20
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-13 20:33:38 -0300

    Sending quiz and survey on schedules

--- src/bootstrap/schedules.js
+++ src/bootstrap/schedules.js
@@ -14,15 +14,22 @@ export async function schedules ({
 }) {
   const { body: { data: schedules } } = await StrabotManager.get('schedules', {
     searchParams: {
-      populate: 'Messages',
+      populate: 'Messages,Quizzes.Answers,Surveys.Options',
       'filters[Telegram][$eq]': true,
       'filters[$or][0][Date][$gt]': dayjs().toISOString(),
-      'filters[$or][1][Date][$null]': true
+      'filters[$or][1][Cron][$notNull]': true
     }
   })
 
   for (const schedule of schedules) {
-    const { Recurrent, Date: date, Cron, Messages } = schedule.attributes
+    const {
+      Recurrent,
+      Date: date,
+      Cron,
+      Messages: { data: messages },
+      Quizzes: { data: quizzes },
+      Surveys: { data: surveys }
+    } = schedule.attributes
 
     Scheduler.schedule({
       when: Recurrent ? Cron : new Date(date),
@@ -32,10 +39,34 @@ export async function schedules ({
         for (const chat of chats) {
           const { Chat_ID: chatId } = chat.attributes
 
-          for (const message of Messages.data) {
+          for (const message of messages) {
             const { Text } = message.attributes
 
             bot.telegram.sendMessage(chatId, Text)
+          }
+
+          for (const quiz of quizzes) {
+            const { Question, Answers } = quiz.attributes
+    
+            const correctAnswerIndex = Answers.findIndex(({ Correct }) => Correct)
+            await bot.telegram.sendQuiz(
+              chatId,
+              Question,
+              Answers.map(({ Value }) => Value),
+              {
+                correct_option_id: correctAnswerIndex
+              }
+            )
+          }
+    
+          for (const survey of surveys) {
+            const { Question, Options } = survey.attributes
+    
+            await bot.telegram.sendPoll(
+              chatId,
+              Question,
+              Options.map(({ Value }) => Value)
+            )
           }
         }
       }
